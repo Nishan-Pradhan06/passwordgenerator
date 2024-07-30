70bb414fec096872d15aa03c88ff3189c32a36df
nishanpradhan <nishanpradhan500@gmail.com>
2024-07-30 14:31:20 +0545

    created passwordViewField

--- lib/view/components/password_field.dart
+++ lib/view/components/password_field.dart
@@ -6,36 +6,33 @@ class PasswordField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Container(
-        height: MediaQuery.of(context).size.height / 4.5,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-            color: BrandColors.kSecondary,
-            borderRadius: BorderRadius.all(Radius.circular(20))),
-        child: const Center(
-          child: Padding(
-            padding: EdgeInsets.all(50.0),
-            // child: TextField(
-            //   controller: controller,
-            //   readOnly: true,
-            //   enableInteractiveSelection: false,
-            //   decoration: const InputDecoration(
-            //     border: InputBorder.none,
-            //     labelStyle: TextStyle(
-            //         fontSize: 30,
-            //         color: Colors.white,
-            //         fontWeight: FontWeight.bold),
-            //   ),
-            // ),
-            child: Text(
-              "dasdasdas",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 30,
-              ),
+    return Container(
+      height: MediaQuery.of(context).size.height / 4.5,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+          color: BrandColors.kSecondary,
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      child: const Center(
+        child: Padding(
+          padding: EdgeInsets.all(50.0),
+          // child: TextField(
+          //   controller: controller,
+          //   readOnly: true,
+          //   enableInteractiveSelection: false,
+          //   decoration: const InputDecoration(
+          //     border: InputBorder.none,
+          //     labelStyle: TextStyle(
+          //         fontSize: 30,
+          //         color: Colors.white,
+          //         fontWeight: FontWeight.bold),
+          //   ),
+          // ),
+          child: Text(
+            "dasdasdas",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 30,
             ),
           ),
         ),
